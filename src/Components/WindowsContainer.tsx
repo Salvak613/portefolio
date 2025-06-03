cee5775b172bbe30ee45aab273008588673642ac
Jules Clauwaert <jules.clauwaert@gmail.com>
2025-06-03 17:36:58 +0200

    fixed

--- src/Components/WindowsContainer.tsx
+++ src/Components/WindowsContainer.tsx
@@ -59,6 +59,51 @@ const WindowsContainer: React.FC<{
             className={styles.exemple}
           />
           <br />
+          <h3>Projet 3 - EfDouze</h3>
+          <a
+            href="https://efdouze.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://efdouze.netlify.app/
+          </a>
+          <p>
+            Context : 2 jours pour faire un petit jeu en react en rapport plus
+            ou moins avec le dev.
+            <br />
+            Nous avons donc un jeu d'énigme dont le but est tout simplement de
+            trouver le mot de passe, très peu d'indice le but étant de bucher
+            ensemble sur chacune d'entre elles, le début est facile parfois
+            tordu. Plus vous avancez plus il faudra penser outside the box et
+            fouiller un peu.
+          </p>
+          <img
+            src="/stock img/efdouze.PNG"
+            alt="PokeClicker"
+            className={styles.exemple}
+          />
+
+          <h3>Projet 4 - Le Chineur</h3>
+          <p>
+            Context : 1 mois pour créer un site fonctionnel basé sur le front
+            avec une ou plusieurs uttilisation d'un fetch. <br />
+            Pour ce projet nous étions 4 et nous sommes partis sur un site qui
+            se sert de l'API spotify, le principe est simple : Détourner
+            l'algorythme de recherche de Spotify qui se base sur des critères
+            cachés de popularitée pour axer la recherche sur les petits
+            artistes.
+            <br />
+            Il est possible de se connecter à son compte spotify, de faire une
+            recherche basée sur des tags que l'on placera dans la bulle d'idée
+            centrale et d'un filtre de popularité. Une deuxième page nous
+            affiche un player SDK spotify, avec toutes les infos et la
+            navigation necessaire pour écouter une playlist automatique, ajouter
+            des morceaux à ses favoris etc. (cf vidéo ci-dessous)
+          </p>
+          <video controls width="250">
+            <source src="/stock img/chineur.mp4" type="video/webm" />
+          </video>
+
           <p></p>
         </div>
       </DraggableWindow>
